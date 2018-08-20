b2d08c2a51282534b7979e2847b3775d67b63080
rnantume <nnrobin37@gmail.com>
2018-08-20 12:26:37 +0300

    test api can add new question

---
+++ tests/test_questions.py
@@ -0,0 +1,47 @@
+ import unittest
+import json
+
+from app import app, create_app
+from instance.config import app_config
+
+class QuestionsTestCase(unittest.TestCase):
+    """class to represent questions test case"""
+
+    def setUp(self):
+        """setup test variables"""
+        self.app = create_app(config_name='testing')
+        self.client = self.app.test_client()
+        
+        self.question = {'Topic':'Sample Topic', 'Description':"Sample Description"}
+
+
+    def test_hello(self):
+        """Test API can get hello function"""
+        res = self.client().get('/hello')
+        self.assertEqual(res.status_code, 200)
+
+    
+    def test_api_can_get_all_questions(self):
+        """Test API can get all questions (GET request)."""
+        res = self.client().post('/questions', data=self.question)
+        self.assertEqual(res.status_code, 201)
+        res = self.client().get('/questions')
+        self.assertEqual(res.status_code, 200)
+        self.assertIn('Description', str(res.data))
+
+    def test_add_question(self):
+        """Test API can add new question"""
+        res = self.client().post('/questions', data=self.question,
+                                 content_type='application/json')
+        self.assertEqual(res.status_code, 201)
+        self.assertIn('Sample Description', str(res.data))
+
+        
+    def tearDown(self):
+        """teardown initialised variables"""
+        self.question = {}
+        self.answer = {}
+        self.partial_qn = {}
+
+if __name__ =='__main__':
+    unittest.main()
